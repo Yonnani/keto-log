2d50555f1434053b490abbcc13507e92d934da08
Yonnani <iamai5206@gmail.com>
2021-05-20 01:22:34 +0900

    로그인 영역 추가, 기능 추가 중

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 console.log('test');
+
+document.getElementById('login-form').addEventListener('submit', e => {
+    console.log('login clicked', e);
+    e.preventDefault();
+    // firebase.auth().signInWithEmailAndPassword()
+});
